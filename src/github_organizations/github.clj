1bbac8e2d2c05940bfb3d0fc357745b873d2ddc4
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-11-25 16:06:42 -0500

    add functions to get members in org and orgs for member

--- src/github_organizations/github.clj
+++ src/github_organizations/github.clj
@@ -3,7 +3,20 @@
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]))
 
-(defn organization-members
+(defn members-in-organization
   "Get all the members of an organization."
-  [org-name]
-  (client/get (join "/" ["https://api.github.com/orgs" org-name "members"])))
+  [organization]
+  (let [{body :body
+         headers :headers}
+        (client/get (join "/" ["https://api.github.com/orgs" organization "members"]))]
+    {:body body, :headers headers}))
+
+(defn organizations-by-member
+  "Get all the organizations a member belongs to."
+  [member]
+  (let [{body :body
+         headers :headers}
+        (client/get (join "/" ["https://api.github.com/users" member "orgs"]))]
+    {:body body, :headers headers}))
+
+;(def e (json/read-str (apply :body example) :key-fn keyword))
