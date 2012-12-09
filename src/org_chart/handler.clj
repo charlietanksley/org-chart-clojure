d3e3340d67b005aa707bcdb3d879cec13bf597ec
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-12-09 14:18:05 -0500

    have a more helpful homepage message

--- src/org_chart/handler.clj
+++ src/org_chart/handler.clj
@@ -7,12 +7,20 @@
             [org-chart.users :as users]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "<h1>Org-Chart</h1><p>To get the info on an
+  organization, append <code>'/api/<org-name>.json'</code> to the
+  address above. But be aware that for the time being, since we aren't
+  using any sort of authentication, Github will cap your usage at 60
+  requests an hour. If you look for a big organization, you'll use
+  those up before you get one JSON return value back. So look for
+  something small (e.g., rspec).</p>")
+
   (GET "/api/:organization.json" [organization]
        (let [response {:organization (github/full-information-on-organization organization)
                        :members (github/members-in-organization organization)
                        :related-orgnizations (users/all-organizations-for-all-users organization)}]
          (json/write-str response)))
+
   (route/not-found "Not Found"))
 
 (def app
