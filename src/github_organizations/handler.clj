0bae1541778a1061af9b0d00f09ecf85ed5babda
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-12-09 12:41:28 -0500

    add main route

--- src/github_organizations/handler.clj
+++ src/github_organizations/handler.clj
@@ -1,11 +1,20 @@
 (ns github-organizations.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.data.json :as json]
+            [github-organizations.github :as github]
+            [github-organizations.users :as users]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/api/:organization" [organization]
+       (let [response {:organization (github/full-information-on-organization organization)
+                       :members (github/members-in-organization organization)
+                       :related-orgnizations (users/all-organizations-for-all-users organization)}]
+         (json/write-str response)))
   (route/not-found "Not Found"))
 
 (def app
   (handler/site app-routes))
+
